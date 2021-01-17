78e39d87537f2cb707879f3e782b29c5e66832e8
Jingga Saka Asa <jinggandroid.jsa@gmail.com>
2021-01-17 20:33:05 +0700

    stack exception

---
+++ src/com/company/StackTryExceptions.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class StackTryExceptions {
+    public static void main(String args[]) {
+        Stack stack = new Stack();
+        try {
+            stack.push("1");
+            stack.push("2");
+            System.out.println(stack.pop().toString());
+            stack.push("3");
+            stack.push("4");
+            System.out.println(stack.pop().toString());
+            stack.push("5");
+            stack.push("6");
+            stack.push("7");
+            stack.push("8");
+            stack.push("9");
+            stack.push("4");
+        } catch (EmptyStackException e) {
+            System.out.println(e.toString());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            System.out.println("Yang ini selalu dijalankan");
+        }
+
+    }
+}
